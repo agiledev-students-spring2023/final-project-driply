bac6493881c61071937dc33500fd6fd53b705850
MannySotoRuiz <sotoruiz.emmanuel@gmail.com>
2023-03-02 17:28:53 -0500

    created ui to display user's bookmark list

--- front-end/src/components/BookmarksPage.js
+++ front-end/src/components/BookmarksPage.js
@@ -1,8 +1,85 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
 
 function Bookmarks() {
+
+  const [bookmarkList, setBookmarkList] = useState([]);
+  const [bookmarkError, setBookmarkError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchBookmarkList() {
+      const response = await fetch("https://my.api.mockaroo.com/bookmark_schema.json?key=90e03700");
+      let json = await response.json();
+      if (response.status === 200) {
+        setBookmarkList(json);
+        setBookmarkError(null);
+        setLoading(false);
+      } else {
+        setBookmarkError(response.status);
+        setLoading(false);
+      }
+    }
+
+    fetchBookmarkList();
+  }, []);
+
+
+  function LoadingBookmarkList() {
+    return (
+      Array.from({length: 4}).map((_, idx) => {
+        return (
+          <div key={idx} className="bookmarkItem" style={{ backgroundColor: "#DDDDDD", borderRadius: "2%" }}></div>
+        );
+      })
+    );
+  }
+
+  function BookmarkItem({ bookmark }) {
+    return (
+      <div className="bookmarkItem">
+        <div className="bookmarkIcon">
+          <BookmarkIcon sx={{ height: "40px", width: "40px" }} />
+        </div>
+        <div className="bookmarkImg">
+          <img src={bookmark.image} alt="bookmark img"/>
+        </div>
+        <div className="bookmarkTitle">{bookmark.title}</div>
+      </div>
+    );
+  }
+
+  function DisplayBookmarkList() {
+    return (
+      <>
+        {bookmarkList.map((item) => <BookmarkItem key={item.id} bookmark={item}/>)}
+      </>
+    )
+  }
+
   return (
-    <div>Bookmarks</div>
+    <div className="bookmarkPage">
+
+      {/* header */}
+      <div className="bookmarkPageHeader">
+        <h1>Bookmarks</h1>
+      </div>
+
+      {/* body */}
+      <div className="displayAllBookmarks">
+        {/* <div className="bookmarkItem"></div>
+        <div className="bookmarkItem"></div> */}
+
+        {loading ? (
+          <LoadingBookmarkList />
+        ) : (
+          <DisplayBookmarkList />
+        )}
+        {bookmarkError && <div className="error">{bookmarkError}</div>}
+      </div>
+
+
+    </div>
   )
 }
 
