a522a34d87f34e991855b60c6ff56816729d7110
MannySotoRuiz <sotoruiz.emmanuel@gmail.com>
2023-03-04 15:29:24 -0500

    added user interaction with user for likes and bookmark on post

--- front-end/src/components/Post.js
+++ front-end/src/components/Post.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-// bookmark icon for when user does not have it bookmarked
-import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
-// icon for when user bookmarked icon
-// import BookmarkIcon from '@mui/icons-material/Bookmark'; <--- don't delete, will use later
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder'; // not bookmarked
+import BookmarkIcon from '@mui/icons-material/Bookmark'; // bookmarked
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'; // unliked post
-// import FavoriteIcon from '@mui/icons-material/Favorite'; // liked post
-import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
+import FavoriteIcon from '@mui/icons-material/Favorite'; // liked post
+import ChatBubbleIcon from '@mui/icons-material/ChatBubble'; // comment icon
 
 function Post({ post }) {
+
+    const [ifBookmarked, setIfBookmarked] = useState(post.bookmarked);
+    const [ifLiked, setIfLiked] = useState(post.liked);
+
+    const handleBookmarkClick = () => {
+        post.bookmarked = !ifBookmarked;
+        setIfBookmarked(!ifBookmarked);
+    }
+    const handlePostLike = () => {
+        post.liked = !ifLiked;
+        setIfLiked(!ifLiked);
+
+        // update like count
+        if (post.liked) {
+            post.likes += 1;
+        } else {
+            post.likes -= 1;
+        }
+    }
+
     return (
         <div className="post">
 
@@ -28,12 +46,26 @@ function Post({ post }) {
                 {/* <img src={post.post_picture} alt="post img"/> */}
                 <img src="https://picsum.photos/300/300" alt="postpic"/>
                 <div className="postBookmarkIcon">
-                    <BookmarkBorderIcon sx={{ height: "50px", width: "50px" }} />
+                    {ifBookmarked ? (
+                        <BookmarkIcon 
+                            onClick={handleBookmarkClick}
+                            sx={{ height: "50px", width: "50px", color: "white" }} 
+                        />
+                    ) : (
+                        <BookmarkBorderIcon 
+                            onClick={handleBookmarkClick} 
+                            sx={{ height: "50px", width: "50px" }} 
+                        />
+                    )}
                 </div>
                 {/* post likes, comments */}
                 <div className="postDetails">
                     <div>
-                        <FavoriteBorderIcon />
+                        {ifLiked ? (
+                            <FavoriteIcon onClick={handlePostLike} sx={{ color: "pink" }} />
+                        ) : (
+                            <FavoriteBorderIcon onClick={handlePostLike} />
+                        )}
                         <p>{post.likes}</p>
                     </div>
                     <div>
@@ -44,7 +76,7 @@ function Post({ post }) {
             </div>
 
         </div>
-    )
+    );
 }
 
-export default Post
+export default Post;
