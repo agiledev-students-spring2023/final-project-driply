011e3124bfec63d2032f7e6616e8437686b3fc5f
b-chen00 <bc2982@nyu.edu>
2023-03-03 23:16:52 -0500

    added poster pfp and username as well as cost placeholder to post page

--- front-end/src/pages/PostPage.js
+++ front-end/src/pages/PostPage.js
@@ -1,4 +1,5 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate, useParams } from 'react-router-dom';
 //import { Link } from 'react-router-dom';
 
 // const required = (value) => {
@@ -12,28 +13,63 @@ import React, { useEffect } from "react";
 // };
 
 const PostPage = () => {
-    //const [comment, setComment] = useState("");
-    //const [post, setPost] = useState(null);
+    const navigate = useNavigate();
+    const [comment, setComment] = useState("");
+    const [post, setPost] = useState(null);
+    const [commentList, setCommentList] = useState([]);
+    const [postError, setPostError] = useState(null);
+    const { id } = useParams();
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        // Retrieve post information here and update post using setPost
+        async function fetchPostInfo() {
+            const response = await fetch(``, {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    "PostId": id
+                })
+            });
+            let json = await response.json();
+            if (response.status === 200) {
+                console.log(json);
+                setPostError(null);
+                setLoading(false);
+            } else {
+                setPostError(response.status);
+                setLoading(false);
+            }
+        }
+
+        fetchPostInfo();
     }, []);
 
-    // const onChangeComment = (e) => {
-    //     setComment(e.target.value);
-    // };
+    const onChangeComment = (e) => {
+        setComment(e.target.value);
+    };
 
-    // const handleComment = (e) => {
-    //     e.preventDefault();
+    const handleComment = (e) => {
+        e.preventDefault();
 
 
-    // };
+    };
 
     return (
         <div>
-            <div className="container">
-                <img className="center-block img-responsive" src="https://picsum.photos/id/24/131/150" alt="pic"/>
+            <div className="row align-items-center" style={{margin: 'auto'}}>
+                <div className="col-8 d-flex align-items-center ps-0 ">
+                    <div onClick={() => navigate("/profile")} className="followingImg">
+                        <img src="https://picsum.photos/id/64/200" alt="user img"/>
+                    </div>
+                    <span onClick={() => navigate("/profile")}>Username</span>
+                </div>
+                <div className="col-4 d-flex justify-content-end">
+                    <span className="mr-3">Cost</span>
+                </div>
             </div>
+            <img className="center-block img-responsive" src="https://picsum.photos/id/24/131/150" alt="pic"/>
         </div>
     );
 };
