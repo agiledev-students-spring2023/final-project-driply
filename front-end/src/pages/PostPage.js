fd082307a1e3f11d5bc3e3782d98ee33e036e824
b-chen00 <bc2982@nyu.edu>
2023-03-04 03:43:54 -0500

    created responsive like button

--- front-end/src/pages/PostPage.js
+++ front-end/src/pages/PostPage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, useParams } from 'react-router-dom';
+import { useAuthContext } from '../hooks/useAuthContext';
 //import { Link } from 'react-router-dom';
 
 // const required = (value) => {
@@ -17,13 +18,17 @@ const PostPage = () => {
     const [comment, setComment] = useState("");
     const [post, setPost] = useState(null);
     const [commentList, setCommentList] = useState([]);
+    const [likes, setLikes] = useState([]);
+    const [likeChanged, setLikeChanged] = useState(false);
     const [postError, setPostError] = useState(null);
     const { id } = useParams();
     const [loading, setLoading] = useState(true);
+    const { user } = useAuthContext();
 
     useEffect(() => {
+        setLikeChanged(false);
         async function fetchPostInfo() {
-            const response = await fetch(``, {
+            const response = await fetch(`url`, {
                 method: "POST",
                 headers: {
                     'Content-Type': 'application/json'
@@ -44,7 +49,7 @@ const PostPage = () => {
         }
 
         fetchPostInfo();
-    }, []);
+    }, [likeChanged]);
 
     const onChangeComment = (e) => {
         setComment(e.target.value);
@@ -53,23 +58,54 @@ const PostPage = () => {
     const handleComment = (e) => {
         e.preventDefault();
 
+    };
 
+    const handleLike = () => {
+        if (likes.filter(e => (e.localeCompare(user.username) === 0)).length === 0){
+            likes.push(user.username);
+        }
+        else{
+            const index = likes.indexOf(user.username);
+            likes.splice(index, 1);
+        }
+        setLikeChanged(true);
+        console.log(likes);
     };
 
     return (
-        <div>
-            <div className="row align-items-center" style={{margin: 'auto'}}>
-                <div className="col-8 d-flex align-items-center ps-0 ">
+        <div className="mb-10">
+            <div className="row align-items-center mx-auto">
+                <div className="col-8 d-flex align-items-center px-0 mx-auto">
                     <div onClick={() => navigate("/profile")} className="followingImg">
                         <img src="https://picsum.photos/id/64/200" alt="user img"/>
                     </div>
                     <span onClick={() => navigate("/profile")}>Username</span>
                 </div>
-                <div className="col-4 d-flex justify-content-end">
+                <div className="col-4 d-flex justify-content-end px-0 mx-auto">
                     <span className="mr-3">Cost</span>
                 </div>
             </div>
             <img className="center-block img-responsive" src="https://picsum.photos/id/24/131/150" alt="pic"/>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum scelerisque egestas hendrerit. Nam in ornare metus. Sed gravida vel orci vitae gravida. Nulla hendrerit, quam et varius posuere, orci lacus condimentum ipsum, at maximus nisi arcu sit amet erat. Nulla gravida malesuada orci, eget vestibulum dolor suscipit et. Donec sit amet nisi ac lacus finibus sollicitudin. Donec aliquet malesuada iaculis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Mauris placerat mi tincidunt libero mollis, vitae tempor arcu consectetur. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus tempus est lacus, sit amet vehicula enim dapibus dictum. Nam ultricies congue urna, non finibus odio placerat rhoncus. Sed dui dui, blandit et luctus vitae, lobortis a nulla. Mauris vel rhoncus odio, tempus pharetra odio. Aenean purus justo, dignissim in pellentesque lobortis, viverra nec erat. Nunc accumsan interdum eros a volutpat.
+            <br/>
+            {likes.length}
+            {likes.filter(e => (e.localeCompare(user.username) === 0)).length === 0 && (
+                <button
+                    onClick={() => handleLike()}
+                    className='btn btn-secondary mx-2'
+                >
+                    Like
+                </button>
+            )}
+            {likes.filter(e => (e.localeCompare(user.username) === 0)).length > 0 && (
+                <button
+                    onClick={() => handleLike()}
+                    className='btn btn-success mx-2'
+                >
+                    Unlike
+                </button>
+            )}
+            <div className="pb-5">hello</div>
         </div>
     );
 };
