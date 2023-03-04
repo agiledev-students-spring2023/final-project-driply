f0dfb9812ff5eff414104e3b8504cc37cfb38f5a
MannySotoRuiz <sotoruiz.emmanuel@gmail.com>
2023-03-04 13:19:46 -0500

    used icons for bottom nav bar and responsive

--- front-end/src/components/BottomNavBar.js
+++ front-end/src/components/BottomNavBar.js
@@ -1,25 +1,36 @@
 import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import TrendingUpOutlinedIcon from '@mui/icons-material/TrendingUpOutlined';
+import AddBoxOutlinedIcon from '@mui/icons-material/AddBoxOutlined';
+import AccountBoxOutlinedIcon from '@mui/icons-material/AccountBoxOutlined';
+import HomeIcon from '@mui/icons-material/Home';
+import AddBoxIcon from '@mui/icons-material/AddBox';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
 
 function BottomNavBar() {
+  const navigate = useNavigate();
   let location = useLocation();
   let splitPath = location.pathname.split("/");
+
+  // hide navbar when needed
   let ifHide;
   if (splitPath.includes("chatroom")) {
     ifHide = true;
   } else {
     ifHide = false;
   }
+
   return (
     <>
       {ifHide ? (
         <div></div>
       ) : (
         <div className="bottomNavBar">
-          <Link to="/">Home</Link>
-          <Link to="/">Trending</Link>
-          <Link to="/postform">Post</Link>
-          <Link to="/profile">Profile</Link>
+          <div onClick={() => navigate("/")}>{(location.pathname === "/") ? <HomeIcon fontSize="large"/> : <HomeOutlinedIcon />}</div>
+          <div onClick={() => navigate("/trending")}><TrendingUpOutlinedIcon fontSize={(location.pathname === "/trending") ? "large" : "medium"} /></div>
+          <div onClick={() => navigate("/")}>{(location.pathname === "/post") ? <AddBoxIcon fontSize="large"/> : <AddBoxOutlinedIcon />}</div>
+          <div onClick={() => navigate("/profile")}>{(location.pathname === "/profile") ? <AccountBoxIcon fontSize="large"/> : <AccountBoxOutlinedIcon />}</div>
         </div>
       )}
     </>
