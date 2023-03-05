5c40fcac08dfb5a5b47701f01382c35359dc4360
b-chen00 <bc2982@nyu.edu>
2023-03-05 17:24:42 -0500

    comment pfp size is now maintained with comment length

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 import "./styles/index.css";
 import "./styles/chats.css";
 import "./styles/profilepage.css";
@@ -8,8 +9,10 @@ import "./styles/following.css";
 import "./styles/follower.css";
 import "./styles/postform.css";
 import "./styles/bookmarks.css";
+import './styles/post.css';
 import { AuthContextProvider } from "./context/AuthContext";
 import App from "./App";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
