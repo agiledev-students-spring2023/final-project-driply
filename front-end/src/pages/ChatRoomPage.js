f97c061f035d9d168c9ff931122ac9105cc39822
MannySotoRuiz <sotoruiz.emmanuel@gmail.com>
2023-03-07 18:39:39 -0500

    changed chat room css and added responsive submit btn

--- front-end/src/pages/ChatRoomPage.js
+++ front-end/src/pages/ChatRoomPage.js
@@ -1,19 +1,17 @@
-import React from 'react';
-import { useParams, useLocation, useNavigate } from 'react-router-dom';
-import Input from "@mui/joy/Input";
+import React, { useState, useRef } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import SendIcon from '@mui/icons-material/Send';
 
 function ChatRoomPage() {
     const navigate = useNavigate();
-    const { chatId } = useParams();
+    // const { chatId } = useParams();
     const location = useLocation();
     const firstName = location.state.name;
     const senderImg = location.state.senderImg;
-    console.log(senderImg);
-    console.log("chatId: ", chatId, firstName);
+    // console.log("chatId: ", chatId, firstName);
 
-    const messages = [
+    const tempMessages = [
         {
             id: 1,
             from: "me",
@@ -45,13 +43,15 @@ function ChatRoomPage() {
             message: "I'm good"
         },
     ]
-
+    const [messages, setMessages] = useState(tempMessages);
+    const inputRef = useRef();
+    const onInput = () => inputRef.current.value;
 
     function SenderMessage({ message, idx }) {
         const bottomMargin = (idx === (messages.length - 1)) ? "20px" : "0px";
         return (
             <div className="senderMessage" style={{ marginBottom: bottomMargin }}>
-                <div style={{ color: "white" }}>{message.message}</div>
+                <div className="senderMessageContent" style={{ color: "white" }}>{message.message}</div>
             </div>
         );
     }
@@ -60,16 +60,31 @@ function ChatRoomPage() {
         const bottomMargin = (idx === (messages.length - 1)) ? "20px" : "0px";
         return (
             <div style={{ display: "flex", flexDirection: "row", paddingTop: "2px", marginBottom: bottomMargin }}>
-                <img className="receiverImg" src={senderImg} width="50px" alt="img"/>
+                <img className="receiverImg" src={senderImg} width="40px" height="40px" alt="img"/>
                 <div className="receiverMessage">{message.message}</div>
             </div>
         );
     }
 
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const typedMessage = inputRef.current.value;
+        if (typedMessage.length === 0) {
+            console.log("empty message");
+            return;
+        }
+        const messageObj = {id: messages.length+1, from: "me", message: typedMessage, new: true};
+        const copiedMessages = [...messages];
+        copiedMessages.push(messageObj);
+        setMessages(copiedMessages);
+        inputRef.current.value = "";
+    }
     function SendBtn() {
         return (
-            <div className="sendChatBtn">
-                <SendIcon />
+            <div className="sendChatBtnDiv">
+                <div onClick={event => handleSubmit(event)} className='sendChatBtn'>
+                    <SendIcon />
+                </div>
             </div>
         );
     }
@@ -80,7 +95,6 @@ function ChatRoomPage() {
             {/* header */}
             <div className="chatRoomHeader">
                 <div onClick={() => {navigate(`/chats`)}}><ArrowBackIcon size="lg" /></div>
-                {/* <h3 onClick={() => {navigate(`/chats`)}}>{backArr}</h3> */}
                 <h1>{firstName}</h1>
             </div>
 
@@ -89,17 +103,20 @@ function ChatRoomPage() {
                 {messages.map((message, idx) => {
                     if (message.from === "me") {
                         return (
-                            <ReceiverMessage key={message.id} message={message} idx={idx}/>
+                            <SenderMessage key={message.id} message={message} idx={idx}/>
                         )
                     } else  {
                         return (
-                            <SenderMessage key={message.id} message={message} idx={idx}/>   
+                            <ReceiverMessage key={message.id} message={message} idx={idx}/>
                         )
                     }
                 })}
             </div>
             <div className="chatroomInput">
-                <Input placeholder="Type in here…" size="lg" variant="soft" endDecorator={<SendBtn />}/>
+                <form>
+                    <input type="text" ref={inputRef} id="chatInput" placeholder="Type in here…" onInput={onInput}/>
+                    <SendBtn className="submitChat" />
+                </form>
             </div>
 
         </div>
