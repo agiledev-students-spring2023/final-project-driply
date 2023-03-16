1e1b7a3fbab870aea93fb17181dc52b3042fcea9
b-chen00 <bc2982@nyu.edu>
2023-03-16 00:10:32 -0400

    added base trending page using home page

--- front-end/src/pages/TrendingPage.js
+++ front-end/src/pages/TrendingPage.js
@@ -1,19 +1,73 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import Post from "../components/Post";
 import { DarkModeContext } from '../context/DarkModeContext';
 import { useAuthContext } from '../hooks/useAuthContext';
 
 function TrendingPage() {
   const { user } = useAuthContext();
+  const [postList, setPostList] = useState([]);
+  const [postListError, setPostListError] = useState(null);
+  const [loading, setLoading] = useState(true);
   const { ifDarkMode } = useContext(DarkModeContext);
 
+  useEffect(() => {
+    async function fetchPostList() {
+      const response = await fetch(
+        `https://my.api.mockaroo.com/post_schema.json?key=099dde30`
+      );
+      let json = await response.json();
+      if (response.status === 200) {
+        setPostList(json);
+        setPostListError(null);
+      } else {
+        setPostListError({ error: json.error, status: response.status });
+      }
+      setLoading(false);
+      console.log(json);
+    }
+
+    fetchPostList();
+  }, []);
+
+  function LoadingPosts() {
+    return Array.from({ length: 4 }).map((_, idx) => {
+      return (
+        <div key={idx} className="post">
+          {/* header */}
+          <div className="postHeader">
+            <div className="loadingUserImg"></div>
+          </div>
+
+          {/* post pictures */}
+          <div className="loadingPostBody"></div>
+        </div>
+      );
+    });
+  }
+
+  function DisplayPostLists() {
+    return (
+      <>
+        {postList?.map((post) => (
+          <Post key={post.id} post={post} />
+        ))}
+      </>
+    );
+  }
+
+
   return (
-    <div className={`trendingPage ${ifDarkMode && "darkTheme"}`}>
-      TrendingPage
-      {user && (
-        <div>Hello {user.username}!</div>
+    <div className={`postContainer ${ifDarkMode && "darkTheme"}`}>
+      {postListError && (
+        <div>
+          <h1 className="error">Error Code: {postListError.status}</h1>
+          <h3 className="error">{postListError.error}</h3>
+        </div>
       )}
+
+      {loading ? <LoadingPosts /> : <DisplayPostLists />}
     </div>
-  )
+  );
 }
 
 export default TrendingPage
