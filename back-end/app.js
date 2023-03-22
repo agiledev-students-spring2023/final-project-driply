c78e87514d13154a95cc1292ec052d3799eb41c1
DarrenLe20 <89809192+DarrenLe20@users.noreply.github.com>
2023-03-22 12:41:31 -0400

    setup multer route and passport

--- back-end/app.js
+++ back-end/app.js
@@ -2,10 +2,41 @@ require("dotenv").config({ silent: true });
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const multer = require("multer");
 const mongoose = require("mongoose");
 
+// Set up Express app
 const app = express();
 app.use(morgan("dev", { skip: (req, res) => process.env.NODE_ENV === "test" }));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Multer
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/post-form", upload.single("image"), (req, res) => {
+  if (req.file) {
+    const data = {
+      name: req.body.name,
+      price: req.body.price,
+      description: req.body.description,
+      image: req.file.filename,
+    };
+    res.json(data);
+  } else throw "error";
+});
+
+module.exports = app;
--- front-end/src/pages/PostForm.js
+++ front-end/src/pages/PostForm.js
@@ -63,7 +63,12 @@ function PostForm() {
       <div className="chatPageHeader">
         <h1>Create Post</h1>
       </div>
-      <form className="post-form" onSubmit={handleSubmit}>
+      <form
+        className="post-form"
+        onSubmit={handleSubmit}
+        method="POST"
+        action="post-form"
+      >
         <label className="input-label">
           Upload your photos
           <input
