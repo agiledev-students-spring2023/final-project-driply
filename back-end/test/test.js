e3af1d0a29f082d327a7032b5ac22cfd2356cd4a
b-chen00 <bc2982@nyu.edu>
2023-04-01 19:11:32 -0400

    wrote test for homepage

--- back-end/test/test.js
+++ back-end/test/test.js
@@ -9,16 +9,17 @@ let should = chai.should();
 chai.use(chaiHttp);
 
 
-describe("GET request to /foo route", () => {
+describe("GET request to /getHomePosts route", () => {
     it("it should respond with an HTTP 200 status code and an object in the response body", done => {
       chai
         .request(server)
         .get("/getHomePosts")
         .end((err, res) => {
-          res.should.have.status(200) // use should to make BDD-style assertions
-          res.body.should.be.a("object") // our route sends back an object
-          //res.body.should.have.property("success", true) // a way to check the exact value of a property of the response object
-          done() // resolve the Promise that these tests create so mocha can move on
+          res.should.have.status(200)
+          res.body.should.be.a("object")
+          res.body.should.have.property("data")
+          res.body.data.should.be.an("array")
+          done()
         })
     })
 })
