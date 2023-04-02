dfc4c3756668c9a6416738f54b6cfd48e470c9e9
DarrenLe20 <89809192+DarrenLe20@users.noreply.github.com>
2023-04-01 22:02:36 -0400

    tests for like, follower, post

--- back-end/test/test.js
+++ back-end/test/test.js
@@ -1,25 +1,129 @@
-process.env.NODE_ENV = 'test';
+process.env.NODE_ENV = "test";
 
-let chai = require('chai');
-let chaiHttp = require('chai-http');
-let server = require('../app');
-let should = chai.should();
-
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const server = require("../app");
+const should = chai.should();
+const fs = require("fs");
+const expect = chai.expect;
+const path = require("path");
 
 chai.use(chaiHttp);
 
-
 describe("GET request to /getHomePosts route", () => {
-    it("it should respond with an HTTP 200 status code and an object in the response body", done => {
-      chai
-        .request(server)
-        .get("/getHomePosts")
-        .end((err, res) => {
-          res.should.have.status(200)
-          res.body.should.be.a("object")
-          res.body.should.have.property("data")
-          res.body.data.should.be.an("array")
-          done()
-        })
-    })
-})
+  it("it should respond with an HTTP 200 status code and an object in the response body", (done) => {
+    chai
+      .request(server)
+      .get("/getHomePosts")
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a("object");
+        res.body.should.have.property("data");
+        res.body.data.should.be.an("array");
+        done();
+      });
+  });
+});
+
+describe("GET request to /following route", () => {
+  it("it should respond with an HTTP 200 status code and an object in the response body", (done) => {
+    chai
+      .request(server)
+      .get("/following")
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a("object");
+        res.body.should.have.property("data");
+        res.body.data.should.be.an("array");
+        done();
+      });
+  });
+});
+
+describe("GET request to /follower route", () => {
+  it("it should respond with an HTTP 200 status code and an object in the response body", (done) => {
+    chai
+      .request(server)
+      .get("/follower")
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a("object");
+        res.body.should.have.property("data");
+        res.body.data.should.be.an("array");
+        done();
+      });
+  });
+});
+
+describe("POST request to /post-form route", () => {
+  it("should upload a single file and return its metadata", (done) => {
+    const filePath = __dirname + "/test-image.jpg"; // assuming a test image file is present in the same directory as the test file
+    chai
+      .request(server)
+      .post("/post-form")
+      .attach("image", fs.readFileSync(filePath), "test-image.jpg")
+      .field("name", "Test Product")
+      .field("price", "10.99")
+      .field("description", "This is a test product")
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.an("object");
+        expect(res.body).to.have.property("name").that.equals("Test Product");
+        expect(res.body).to.have.property("price").that.equals("10.99");
+        expect(res.body)
+          .to.have.property("description")
+          .that.equals("This is a test product");
+        expect(res.body).to.have.property("image").that.is.a("string");
+        fs.unlinkSync(
+          path.join(__dirname, "../public/uploads", res.body.image)
+        );
+        done();
+      });
+  });
+
+  it("should throw an error if no file is uploaded", (done) => {
+    chai
+      .request(server)
+      .post("/post-form")
+      .field("name", "Test Product")
+      .field("price", "10.99")
+      .field("description", "This is a test product")
+      .end((err, res) => {
+        expect(res).to.have.status(500);
+        done();
+      });
+  });
+});
+
+describe("POST request to /like/:postID route", () => {
+  it("should return a success message when a post is liked", (done) => {
+    const postID = "1"; // set a dummy post ID for testing purposes
+    chai
+      .request(server)
+      .post(`/like/${postID}`)
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.an("object");
+        expect(res.body).to.have.property("success").that.equals(true);
+        done();
+      });
+  });
+});
+
+describe("POST request to /unlike/:postID route", () => {
+  it("should return a success message when a post is liked", (done) => {
+    const postID = "1"; // set a dummy post ID for testing purposes
+    chai
+      .request(server)
+      .post(`/like/${postID}`)
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.an("object");
+        expect(res.body).to.have.property("success").that.equals(true);
+        done();
+      });
+  });
+});
