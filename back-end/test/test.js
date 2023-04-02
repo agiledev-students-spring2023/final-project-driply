99ea9f14eeb2262cba97502dae28b58567c70b6c
b-chen00 <bc2982@nyu.edu>
2023-04-01 21:34:57 -0400

    added test for fetchcomment and createcomment

--- back-end/test/test.js
+++ back-end/test/test.js
@@ -23,3 +23,39 @@ describe("GET request to /getHomePosts route", () => {
         })
     })
 })
+
+describe("GET request to /fetchComment route", () => {
+  it("it should respond with an HTTP 200 status code and an object in the response body", done => {
+    chai
+      .request(server)
+      .get("/fetchComment")
+      .end((err, res) => {
+        res.should.have.status(200)
+        res.body.should.be.a("object")
+        res.body.should.have.property("username")
+        res.body.username.should.be.an("string")
+        res.body.should.have.property("comments")
+        res.body.comments.should.be.an("array")
+        done()
+      })
+  })
+})
+
+describe("POST request to /createComment route", () => {
+  it("it should respond with an HTTP 200 status code and an object in the response body", done => {
+    var body = {
+      newComment: "hello",
+      postId: 0
+    }
+    chai
+      .request(server)
+      .post("/createComment")
+      .send(body)
+      .end((err, res) => {
+        res.should.have.status(200)
+        res.body.should.be.a("object")
+        res.body.should.have.property("message").eql('success')
+        done()
+      })
+  })
+})
