f9645f1e94a00c36ee0aef07c18629888bd10cbe
kevincwpark <cp3111@nyu.edu>
2023-04-02 21:14:48 -0400

    backend followers

--- front-end/src/pages/FollowerPage.js
+++ front-end/src/pages/FollowerPage.js
@@ -1,36 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
 import { DarkModeContext } from "../context/DarkModeContext";
 
-function FollowerPage() {
-  const [followerList, setFollowerList] = useState([]);
-  const [followerError, setFollowerError] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const { ifDarkMode } = useContext(DarkModeContext);
-
-  useEffect(() => {
-    async function fetchFollowerList() {
-      const response = await fetch(`http://localhost:4000/follower`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      let json = await response.json();
-      if (json.status === 200) {
-        setFollowerList(json.data);
-        setFollowerError(null);
-        setLoading(false);
-        console.log(json);
-      } else {
-        console.log(json.error);
-        setFollowerError({ error: json.error, status: json.status });
-        setLoading(false);
-      }
-    }
-
-    fetchFollowerList();
-  }, []);
-
   function LoadFollowerList() {
     return Array.from({ length: 10 }).map((_, idx) => {
       return (
@@ -40,6 +10,28 @@ function FollowerPage() {
         </div>
       );
     });
+  }
+
+  function Follower({ follower }) {
+    return (
+      <div className="eachFollowerDisplay">
+        <div className="followerImg">
+          <img src={follower.user_img} alt="user img" />
+        </div>
+        <div className="followerDetails">
+          <p>{follower.username}</p>
+          {follower.if_following ? (
+            <div className={`followBtn ${ifDarkMode && "followBtn-dark"}`}>
+              Unfollow
+            </div>
+          ) : (
+            <div className={`unfollowBtn ${ifDarkMode && "unfollowBtn-dark"}`}>
+              Follow
+            </div>
+          )}
+        </div>
+      </div>
+    );
   }
 
   function DisplayFollowerList() {
