4714590d5a296dd04d6c704402a9be1474a98710
kevincwpark <cp3111@nyu.edu>
2023-04-02 21:29:08 -0400

    refactor1

--- front-end/src/pages/FollowingPage.js
+++ front-end/src/pages/FollowingPage.js
@@ -31,59 +31,9 @@ function FollowingPage() {
     fetchFollowingList();
   }, []);
 
-  function LoadingFollowingList() {
-    return Array.from({ length: 6 }).map((_, idx) => {
-      return (
-        <div key={idx} className="eachFollowingDisplay">
-          <div className="followingImgLoading"></div>
-          <div className="followingDetailsLoading"></div>
-        </div>
-      );
-    });
-  }
-
-  function Following({ following }) {
-    return (
-      <div className="eachFollowingDisplay">
-        <div className="followingImg">
-          <img src={following.user_img} alt="user img" />
-        </div>
-        <div className="followingDetails">
-          <p>{following.username}</p>
-          <div className={`unfollowBtn ${ifDarkMode && "unfollowBtn-dark"}`}>
-            Unfollow
-          </div>
-        </div>
-      </div>
-    );
-  }
-
-  function DisplayFollowingList() {
-    return (
-      <div className="followingContainer">
-        {followingList?.map((following) => (
-          <Following key={following.id} following={following} />
-        ))}
-      </div>
-    );
-  }
-
-  return (
-    <div className={ifDarkMode && "darkTheme"}>
-      {/* header */}
-      <div className="chatPageHeader">
-        <h1>Following {followingList?.length}</h1>
-      </div>
-
-      {loading ? <LoadingFollowingList /> : <DisplayFollowingList />}
-      {followingError && (
-        <div>
-          <h1 className="error">{followingError.status}</h1>
-          <h3 className="error">{followingError.error}</h3>
-        </div>
-      )}
-    </div>
-  );
+  
 }
 
 export default FollowingPage;
+
+
