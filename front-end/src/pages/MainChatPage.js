a290fd4469baff6af5b1ca2a3e1c2ee6b2fd7e40
MannySotoRuiz <sotoruiz.emmanuel@gmail.com>
2023-04-17 22:43:51 -0400

    added realtime unseen message count on chat history page

--- front-end/src/pages/MainChatPage.js
+++ front-end/src/pages/MainChatPage.js
@@ -3,56 +3,28 @@ import { useNavigate } from 'react-router-dom';
 import { DarkModeContext } from '../context/DarkModeContext';
 import { useAuthContext } from '../hooks/useAuthContext';
 import { io } from "socket.io-client";
-// import { collection, onSnapshot, query } from 'firebase/firestore';
-// import { dbFirebase } from '../firebase-config';
+import moment from "moment";
 
 function MainChatPage() {
   let navigate = useNavigate();
 
   const [chatsList, setChatLists] = useState([]);
-  const [allChats, setAllChats] = useState([]);
-  const [chatError, setChatError] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const { user } = useAuthContext();
   const { ifDarkMode } = useContext(DarkModeContext);
   const socket = useRef();
   
 
   useEffect(() => {
-    socket.current = io("http://localhost:4000");
-    socket.current.on("test", (data) => {
-      console.log(data);
+    setLoading(true);
+    const getUser = JSON.parse(localStorage.getItem("user"));
+    socket.current = io(`http://localhost:4000?userId=${getUser.id}`);
+    socket.current.on("chatHistory", (data) => {
+      // console.log(data.chatList);
+      setChatLists(data.chatList);
+      setLoading(false);
     });
   }, []);
-
-  useEffect(() => {
-    async function fetchUsersChat () {
-      if (user) {
-        const response = await fetch(`http://localhost:4000/chats`, {
-          method: "GET",
-          headers: {
-              'Content-Type': 'application/json'
-          }
-          // body: JSON.stringify({
-          //     "userId": id
-          // })
-        });
-        let json = await response.json();
-        if (json.status === 200) {
-          const { data } = json;
-          data.sort((a, b) => new Date(a.date_sent) - new Date(b.date_sent)); // sorted based on date
-          setAllChats(data);
-          setChatError(null);
-          setLoading(false);
-        } else {
-          setLoading(false);
-          setChatError({error: json.error, status: json.status });
-        }
-      }
-    }
-    fetchUsersChat();
-  }, [user]);
-
 
   function LoadingChat() {
     return (
@@ -69,55 +41,87 @@ function MainChatPage() {
 
 
   function Chat({ chat }) {
-    const ifRead = chat.read;
-    const today = new Date();
-    const todayFormat = new Date(today.getMonth() + 1  + "/" + today.getDate() + "/" + today.getFullYear());
-    const givenDate = new Date(chat.date_sent);
-    const diff = todayFormat.getTime() - givenDate.getTime();
-    const countedDays = diff / (1000 * 3600 * 24);
-    const daySent = new Date(today);
-    daySent.setDate(daySent.getDate() - countedDays);
-    const getDay = ["Sun", "Mon", "Tue", "Wed", "Thurs", "Fri", "Sat"];
-    let dateDisplay;
-    if (countedDays <= 6 && countedDays >= 1) {
-      dateDisplay = getDay[daySent.getDay()];
-    } else if (countedDays === 0) {
-      dateDisplay = chat.time_sent;
-    } else {
-      dateDisplay = `${daySent.getMonth() + 1}/${daySent.getDate()}/${daySent.getFullYear()}`;
-    }
 
+    const [unseenMessages, setUnseenMessages] = useState([]);
+    const [lastMessage, setLastMessage] = useState("");
+    const [formattedTime, setFormattedTime] = useState("");
+
+    useEffect(() => {
+      socket.current.on("updateChatHistory", (data) => {
+        console.log('new message!');
+        console.log(data.newMessage);
+        if (data.newMessage.chatId === chat.chatId) {
+          const id_from = data.newMessage.id_from;
+          const message = data.newMessage.message;
+          const newMsgObj = { id_from, message };
+          setUnseenMessages((prev) => [...prev, newMsgObj]);
+          setLastMessage(message);
+        }
+      });
+    }, []);
+
+    const getUser = JSON.parse(localStorage.getItem("user"));
+    let receiverName = (getUser.username !== chat.members[0].name) ? chat.members[0].name : chat.members[1].name;
+
+    useEffect(() => {
+      function displayChat() {
+        const copyMessages = [...chat.messages];
+        copyMessages.sort((a, b) => {
+          return new Date(a.createdAt) - new Date(b.createdAt);
+        });
+        setLastMessage(copyMessages[copyMessages.length - 1].message);
+        const createdAt = moment(copyMessages[copyMessages.length - 1].createdAt);
+        const now = moment();
+    
+        // calculate the time difference in hours
+        const hoursDiff = now.diff(createdAt, 'hours');
+    
+        // determine the format based on the time difference
+        let format;
+        if (hoursDiff < 24) {
+          format = 'h:mm A'; // display the hour and minute
+        } else if (hoursDiff < 6 * 24) {
+          format = 'dddd'; // display the day of the week
+        } else {
+          format = 'M/D/YYYY'; // display the date
+        }
+        setFormattedTime(createdAt.format(format));
+      }
+
+      displayChat();
+    }, [chat]);
 
     return (
-      <div onClick={() => {navigate(`/chatroom/${chat.id}`, {state: {name: chat.first_name, senderImg: chat.user_image}})}} className="eachChatDisplay">
+      <div onClick={() => {navigate(`/chatroom/${chat.chatId}`, {state: {name: chat?.first_name, senderImg: chat?.user_image}})}} className="eachChatDisplay">
         
         {/* image */}
         <div className="chatImg">
-          <img src={chat.user_image} alt="username pic"/>
+          <img src="https://picsum.photos/100/100" alt="username pic"/>
         </div>
 
         {/* chat details */}
         <div className="chatDetails">
           {/* Username and time message sent*/}
           <div className="topChatDetails">
-            <h4>{chat.first_name}</h4>
+            <h4>{receiverName}</h4>
 
-            {ifRead ? (
-                <p className="gray">{dateDisplay}</p>
+            {unseenMessages.length !== 0 ? (
+                <p className="gray">{formattedTime}</p>
               ) : (
-                <p>{dateDisplay}</p>
+                <p>{formattedTime}</p>
               )
             }
+            {/* <p className='gray'>{formattedTime}</p> */}
           </div>
 
           {/* last text and notifcations */}
           <div className="bottomChatDetails">
-            {chat.last_text.length < 30 ? (
-              <p>{chat.last_text}</p>
+            {lastMessage.length < 30 ? (
+              <p>{lastMessage}</p>
             ) : (
-              <p>{chat.last_text.substring(0, 30)}...</p>
+              <p>{lastMessage.substring(0, 30)}...</p>
             )}
-            {ifRead ? (null) : (<p>{chat.notifications}</p>)}
+            {unseenMessages.length === 0 ? (null) : (<p>{unseenMessages.length}</p>)}
           </div>
 
         </div>
@@ -129,7 +133,8 @@ function MainChatPage() {
   function DisplayChats() {
     return (
       <>
-        {allChats?.slice(0).reverse().map((chat) => <Chat key={chat.id} chat={chat}/>)}
+        {/* {allChats?.slice(0).reverse().map((chat) => <Chat key={chat.id} chat={chat}/>)} */}
+        {chatsList?.map((chat, idx) => <Chat key={idx} chat={chat}/>)}
       </>
     );
   }
@@ -162,23 +167,12 @@ function MainChatPage() {
           <NotLoggedInDisplay />
         ) : (loading) ? (
           <LoadingChat />
-        ) : (allChats.length === 0 && !chatError) ? (
+        ) : (chatsList.length === 0) ? (
           <h3>No chats</h3>
         ) : (
           <DisplayChats />
         )}
 
-
-        {chatError && user && 
-          <div>
-            <h1 className='error'>
-              {chatError.status}
-            </h1>
-            <h3 className="error">
-              {chatError.error}
-            </h3>
-          </div>
-        }
       </div>
     </div>
   );
