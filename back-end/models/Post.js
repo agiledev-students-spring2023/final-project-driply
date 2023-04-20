83405cc5a7596fab7880582cecc82d3b01a1bc7f
DarrenLe20 <89809192+DarrenLe20@users.noreply.github.com>
2023-04-20 16:42:21 -0400

    fixed model for post

--- back-end/models/Post.js
+++ back-end/models/Post.js
@@ -1,19 +1,25 @@
 const mongoose = require("mongoose");
-const dotenv = require("dotenv")
-dotenv.config()
+const dotenv = require("dotenv");
+dotenv.config();
 
-mongoose.connect('mongodb+srv://' + process.env.MONGO_USERNAME + ':' + process.env.MONGO_PASSWORD + '@driply.rdngwwf.mongodb.net/driply?retryWrites=true&w=majority');
+mongoose.connect(
+  "mongodb+srv://" +
+    process.env.MONGO_USERNAME +
+    ":" +
+    process.env.MONGO_PASSWORD +
+    "@driply.rdngwwf.mongodb.net/driply?retryWrites=true&w=majority"
+);
 
 const PostSchema = new mongoose.Schema({
-	user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   image: String, //tbd
   description: String,
   price: Number,
-  comments: [{type: mongoose.Schema.Types.ObjectId, ref: 'Comment'}],
-  likes: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}]
-
+  bookmarked: Boolean,
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
+  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
 });
 
-const Post = mongoose.model('Post', PostSchema);
+const Post = mongoose.model("Post", PostSchema);
 
-module.exports = Post
+module.exports = Post;
