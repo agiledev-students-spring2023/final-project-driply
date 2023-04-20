c649f64ed65c23cb6cc6dce8012f297ad0f4eb0b
DarrenLe20 <89809192+DarrenLe20@users.noreply.github.com>
2023-04-20 18:56:11 -0400

    backend routes for follow

--- front-end/src/components/Follow.js
+++ front-end/src/components/Follow.js
@@ -1,55 +1,101 @@
 import React, { useState, useEffect } from "react";
-import { useAuthContext } from '../hooks/useAuthContext';
+import { useAuthContext } from "../hooks/useAuthContext";
 
 function Follow(props) {
-    const [followed, setFollowed] = useState(false);
-    const [followedChanged, setFollowedChanged] = useState(false);
-    const [ownProfile, setOwnProfile] = useState(props.ownProfile);
-    const { user } = useAuthContext();
+  const [followed, setFollowed] = useState(false);
+  const [followedChanged, setFollowedChanged] = useState(false);
+  const [ownProfile, setOwnProfile] = useState(props.ownProfile);
+  const { user } = useAuthContext();
 
-    useEffect(() => {
-        setFollowedChanged(false);
-    }, [followedChanged]);
+  useEffect(() => {
+    setFollowedChanged(false);
+  }, [followedChanged]);
 
-    const handleFollow = () => {
-        setFollowed(!followed);
-        setFollowedChanged(true);
-    };
+  const handleFollow = (e) => {
+    if (user) {
+      e.stopPropagation();
 
-    function FollowButton() {
-        if (!followed){
-          return (
-            <button
-              onClick={() => handleFollow()}
-              className="btn btn-secondary buttonPaddingRight"
-            >
-                Follow
-            </button>
-          )
+      async function follow() {
+        const response = await fetch(`http://localhost:4000/follow`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            userID: user._id,
+            followedID: props.id,
+          }),
+        });
+        let json = await response.json();
+        if (response.status === 200) {
+          console.log(json);
+          if (json.message === "success") {
+            setFollowed(true);
+            setFollowedChanged(true);
+          }
         }
-        else{
-          return (
-            <button
-                onClick={() => handleFollow()}
-                className="btn btn-primary buttonPaddingRight"
-            >
-                Followed
-            </button>
-          )
-        }
-    }
-    
-    return (
-      <>
-      { ownProfile ?
-        (
-          <div></div>
-        ) : (
-          <div className="right">{user ? <FollowButton /> : <div></div>}</div>
-        )
       }
-      </>
-    )
+
+      async function unfollow() {
+        const response = await fetch(`http://localhost:4000/unfollow`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            userID: user._id,
+            followedID: props.id,
+          }),
+        });
+        let json = await response.json();
+        if (response.status === 200) {
+          console.log(json);
+          if (json.message === "success") {
+            setFollowed(false);
+            setFollowedChanged(true);
+          }
+        }
+      }
+      if (!followed) {
+        follow();
+      } else {
+        unfollow();
+      }
+      console.log(followed);
+    }
+  };
+
+  function FollowButton() {
+    if (!followed) {
+      return (
+        <button
+          onClick={() => handleFollow()}
+          className="btn btn-secondary buttonPaddingRight"
+        >
+          Follow
+        </button>
+      );
+    } else {
+      return (
+        <button
+          onClick={() => handleFollow()}
+          className="btn btn-primary buttonPaddingRight"
+        >
+          Followed
+        </button>
+      );
+    }
+  }
+
+  return (
+    <>
+      {ownProfile ? (
+        <div></div>
+      ) : (
+        <div className="right">{user ? <FollowButton /> : <div></div>}</div>
+      )}
+    </>
+  );
 }
 
 export default Follow;
