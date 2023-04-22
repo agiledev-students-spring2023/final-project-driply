0e7c20b3d8ee50745ff26276483be94ca8ec9d3b
DarrenLe20 <89809192+DarrenLe20@users.noreply.github.com>
2023-04-22 13:14:33 -0400

    fix post to profile route

--- back-end/app.js
+++ back-end/app.js
@@ -166,6 +166,7 @@ app.post("/getPost", (req, res, next) => {
             likes: p.likes,
             image: p.image,
             pfp: u.profilepic,
+            user: u._id,
           });
         })
         .catch((err) => {
